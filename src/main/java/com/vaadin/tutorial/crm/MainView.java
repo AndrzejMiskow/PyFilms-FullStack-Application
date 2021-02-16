d0a5e70887c16b92f4414fd42fa3042b3f9bdcb4
Andrzej Miskow <sc19am2@leeds.ac.uk>
2021-02-16 18:23:51 +0000

    Testing Basic layouts and creating buttons

--- src/main/java/com/vaadin/tutorial/crm/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/MainView.java
@@ -1,5 +1,9 @@
 package com.vaadin.tutorial.crm;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -9,6 +13,20 @@ public class MainView extends VerticalLayout {
 
     public MainView() {
 
+        //sample application in using verdin#
+
+        //creating new elements
+        Button button = new Button("I'm a button");
+        DatePicker datePicker = new DatePicker("Pick a Date");
+
+        //creating a new layout like qt
+        HorizontalLayout layout = new HorizontalLayout(button,datePicker);
+        layout.setDefaultVerticalComponentAlignment(Alignment.END);
+        add(layout);
+
+
+        //button listener to activiate an action
+        button.addClickListener(clickEvent ->  add(new Text("Clicked: " + datePicker.getValue())));
     }
 
 }
