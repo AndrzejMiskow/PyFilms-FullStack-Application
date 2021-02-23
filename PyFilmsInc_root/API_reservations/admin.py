d5eb7d658847dc6b04a0a1e0648f2f22f0a40650
Andrei Bogdan Vladut <sc19abv@leeds.ac.uk>
2021-02-23 19:37:08 +0000

    Initial implementation of models in API_reservations. Add .gitignore file.

--- PyFilmsInc_root/API_reservations/admin.py
+++ PyFilmsInc_root/API_reservations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Reservation)
+admin.site.register(ReservationType)
+admin.site.register(Screening)
+admin.site.register(Room)
+admin.site.register(Seat)
+admin.site.register(SeatReserved)
--- PyFilmsInc_root/API_reservations/models.py
+++ PyFilmsInc_root/API_reservations/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.timezone import now
+# from API_movies import Movie
+# from API_users import User
 
-# Create your models here.
+
+class Reservation(models.Model):
+    screening_id = models.ForeignKey('Screening', on_delete=models.CASCADE)
+    reservation_type_id = models.ForeignKey('ReservationType', on_delete=models.CASCADE, null=False)
+    reservation_contact = models.CharField(max_length=32, null=False, blank=False)
+    reserved = models.BooleanField(default=False, null=False)
+    paid = models.BooleanField(default=False, null=False)
+    cancelled = models.BooleanField(default=False, null=False)
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ReservationType(models.Model):
+    reservation_type = models.CharField(max_length=32, null=False, blank=False)
+
+
+class Screening(models.Model):
+    # movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    room_id = models.ForeignKey('Room', on_delete=models.CASCADE)
+    screening_start = models.DateTimeField(default=now())
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=32, null=False, blank=False)
+    seats_no = models.IntegerField()
+
+
+class Seat(models.Model):
+    row = models.IntegerField()
+    number = models.IntegerField()
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+class SeatReserved(models.Model):
+    seat_id = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    reservation_id = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    screening_id = models.ForeignKey(Screening, on_delete=models.CASCADE)
+
+
