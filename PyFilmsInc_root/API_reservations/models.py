41caa410a6c03d8b770f971f08edba0062a93874
jakemacgregor <R3b3cc429>
2021-02-24 17:40:19 +0000

    Implemented basic working API interface for reservation data

--- PyFilmsInc_root/API_reservations/models.py
+++ PyFilmsInc_root/API_reservations/models.py
@@ -5,6 +5,7 @@ from django.utils.timezone import now
 
 
 class Reservation(models.Model):
+    objects = models.Manager()
     screening_id = models.ForeignKey('Screening', on_delete=models.CASCADE)
     reservation_type_id = models.ForeignKey('ReservationType', on_delete=models.CASCADE, null=False)
     reservation_contact = models.CharField(max_length=32, null=False, blank=False)
@@ -15,27 +16,32 @@ class Reservation(models.Model):
 
 
 class ReservationType(models.Model):
+    objects = models.Manager()
     reservation_type = models.CharField(max_length=32, null=False, blank=False)
 
 
 class Screening(models.Model):
+    objects = models.Manager()
     # movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     room_id = models.ForeignKey('Room', on_delete=models.CASCADE)
     screening_start = models.DateTimeField(default=now())
 
 
 class Room(models.Model):
+    objects = models.Manager()
     name = models.CharField(max_length=32, null=False, blank=False)
     seats_no = models.IntegerField()
 
 
 class Seat(models.Model):
+    objects = models.Manager()
     row = models.IntegerField()
     number = models.IntegerField()
     room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
 class SeatReserved(models.Model):
+    objects = models.Manager()
     seat_id = models.ForeignKey(Seat, on_delete=models.CASCADE)
     reservation_id = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     screening_id = models.ForeignKey(Screening, on_delete=models.CASCADE)
--- PyFilmsInc_root/API_reservations/urls.py
+++ PyFilmsInc_root/API_reservations/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'reservation', views.ReservationViewSet)
+router.register(r'reservationtype', views.ReservationTypeViewSet)
+router.register(r'screening', views.ScreeningViewSet)
+router.register(r'room', views.RoomViewSet)
+router.register(r'seat', views.SeatViewSet)
+router.register(r'seatreserved', views.SeatReservedViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
