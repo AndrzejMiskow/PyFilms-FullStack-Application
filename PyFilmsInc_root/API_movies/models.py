d13b3fef261333e952b1b414b1ca3eae0cedccbb
Alex Craven <sc19amc@leeds.ac.uk>
2021-02-26 14:55:03 +0000

    movie API connections & removed field from movie table in DB

--- PyFilmsInc_root/API_movies/models.py
+++ PyFilmsInc_root/API_movies/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField(default=0 , primary_key = True)
+    title = models.CharField(max_length=256)
+    director = models.CharField(max_length=256)
+    cast_members = models.CharField(max_length = 256)
+    description = models.CharField(max_length=256)
+    movie_poster = models.ImageField(upload_to='Movie_images', blank = True)
+    movie_duration = models.IntegerField(default = 1)
+    tickets_sold = models.IntegerField(default= 0)
+
+
+    def __str__(self):
+        return self.movie_text
