77fdba715b0a9edd0649e0b4a826591f11628add
Alex Craven <sc19amc@leeds.ac.uk>
2021-02-26 13:31:42 +0000

    added skeleton classes for testing API_reservations

--- PyFilmsInc_root/API_reservations/tests.py
+++ PyFilmsInc_root/API_reservations/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import models.*
 
 # Create your tests here.
+
+class ReservationsTestCase(TestCase):
+    
+    # used to create dummy data, configure DB for testing etc.
+    # prepares test fixture for tests to be conducted
+    def setUp(self):
+        #connect to DB or setup dummy DB with some data in it
+
+
+    #test retrieving reservation entries from DB
+    def test_get_reservation(self):
+    
+    
+    #test adding a reservation to DB
+    def test_put_reservation(self):
+    
