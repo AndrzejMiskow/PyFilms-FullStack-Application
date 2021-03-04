eca5e1f7828be7d68e8fc23422961a5a92ea790f
Alex Craven <sc19amc@leeds.ac.uk>
2021-03-04 22:36:30 +0000

    added certificate field to movie class

--- PyFilmsInc_root/API/models.py
+++ PyFilmsInc_root/API/models.py
@@ -93,6 +93,7 @@ class Movie(models.Model):
     director = models.CharField(max_length=256, null=False, blank=False)
     cast_members = models.CharField(max_length=256, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+    certificate = models.IntegerField(null=False, blank=False)
     # movie_poster = models.ImageField(upload_to='Movie_images', blank=True)
     movie_duration = models.IntegerField(null=False, blank=False)  # in minutes
     tickets_sold = models.IntegerField(default=0)
