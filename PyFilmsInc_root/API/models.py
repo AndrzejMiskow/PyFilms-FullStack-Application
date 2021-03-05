d6a991463849e3fafe830c290050249ca17701e8
Alex Craven <sc19amc@leeds.ac.uk>
2021-03-05 03:20:52 +0000

    configured images for movies

--- PyFilmsInc_root/API/models.py
+++ PyFilmsInc_root/API/models.py
@@ -93,7 +93,9 @@ class Movie(models.Model):
     director = models.CharField(max_length=256, null=False, blank=False)
     cast_members = models.CharField(max_length=256, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    # movie_poster = models.ImageField(upload_to='Movie_images', blank=True)
+    poster_img = models.ImageField(upload_to='static/customer/img/',
+                                   default='static/customer/img/default_movie.jpg',
+                                   blank=False, null=False)
     movie_duration = models.IntegerField(null=False, blank=False)  # in minutes
     tickets_sold = models.IntegerField(default=0)
     certificate = models.CharField(max_length=2, default="U", null=False, blank=False)
