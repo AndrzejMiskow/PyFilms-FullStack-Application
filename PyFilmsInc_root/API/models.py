013c9f7d9510373dabea0a4390abbacb7a439453
jakemacgregor <R3b3cc429>
2021-03-08 23:52:17 +0000

    Resolving more conflicts in branches

--- PyFilmsInc_root/API/models.py
+++ PyFilmsInc_root/API/models.py
@@ -93,9 +93,7 @@ class Movie(models.Model):
     director = models.CharField(max_length=256, null=False, blank=False)
     cast_members = models.CharField(max_length=256, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    poster_img = models.ImageField(upload_to='static/customer/img/',
-                                   default='static/customer/img/default_movie.jpg',
-                                   blank=False, null=False)
+    # movie_poster = models.ImageField(upload_to='Movie_images', blank=True)
     movie_duration = models.IntegerField(null=False, blank=False)  # in minutes
     tickets_sold = models.IntegerField(default=0)
     certificate = models.CharField(max_length=2, default="U", null=False, blank=False)
