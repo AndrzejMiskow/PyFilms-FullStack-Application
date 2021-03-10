735d74f2e696b025dcf2daf09caa0a57c95c71ce
Andrzej Miskow <sc19am2@leeds.ac.uk>
2021-03-10 00:10:09 +0000

    Redone code for selecting seats, updated css, layout and JS to allow easier integration with the database.

--- PyFilmsInc_root/static/customer/js/seatLayout.js
+++ PyFilmsInc_root/static/customer/js/seatLayout.js
@@ -1,16 +1,17 @@
-function onLoaderFunc()
-{
-    $(".seatStructure *").prop("disabled", true);
-    $(".displayerBoxes *").prop("disabled", true);
-}
+//contains all the elements in the seat layout
+const seatLayout = document.querySelector('.seatLayout');
 
+//All seats which are not reserved
+const seats = document.querySelectorAll('.row .seat:not(.reserved)');
+
+//number of tickets
 var totalTickets = 0;
+var selectedSeatsCount = 0;
 
 function takeData()
 {
     //Adding up tickets from the form
     totalTickets = Number($("#qChild").val()) + Number($("#qAdult").val()) + Number($("#qSenior").val());
-
     if (totalTickets <= 0)
     {
         alert("Please select a valid number of tickets");
@@ -20,6 +21,31 @@ function takeData()
         alert("Select Your Seats");
     }
 }
+
+function updateSelected (){
+    const selectedSeats = document.querySelectorAll('.row .selected');
+
+    //We can use seatsIndex to store a seat in the DB as selected (after confirmation)
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    selectedSeatsCount = selectedSeats.length;
+
+};
+
+// Function to select seats, if the number of tickets is bigger than 0
+//The click contains a seat and is not occupied then it will be selected
+seatLayout.addEventListener('click', e => {
+    if (
+        totalTickets > 0 &&
+        selectedSeatsCount < totalTickets &&
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelected();
+    }
+});
 
 
 
