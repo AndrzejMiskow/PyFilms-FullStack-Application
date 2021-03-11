461b2b792ce0f7197a4369a00e0995f5554f7f94
jakemacgregor <R3b3cc429>
2021-03-11 12:44:13 +0000

    Representation of seats is now live form the database

--- PyFilmsInc_root/customer/views.py
+++ PyFilmsInc_root/customer/views.py
@@ -6,7 +6,7 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 from django.core.mail import send_mail, EmailMessage
 
-from API.models import Movie, Reservation, Screening
+from API.models import Movie, Reservation, Screening, Seat, SeatReserved
 
 
 class HomeView(ListView):
@@ -27,15 +27,35 @@ class BuyTickets(ListView):
 def render_purchase_view(request, *args, **kwargs):
     pk = kwargs.get('pk')
     screening = get_object_or_404(Screening, pk=pk)
+    seats = Seat.objects.filter(room_id=screening.room_id)
 
+    # Generating a matrix to represent the seats and whether they're reserved
+    rows = seats.last().row
+    cols = seats.last().number
+    current_col = 0
+    current_row = 0
+    layout = [["seat" for c in range(cols)] for r in range(rows)]
+
+    for seat in seats:
+        seat_reserved = SeatReserved.objects.filter(screening_id=screening, seat_id=seat).count()
+        if seat_reserved != 0:
+            layout[current_row][current_col] = "seat reserved"
+        if current_col == rows - 1:
+            current_row += 1
+            current_col = 0
+        else:
+            current_col += 1
+
+    # Applies the movie title and layout to the template
     template_path = 'buyTickets.html'
     context = {
-        'movie': screening.movie_id.title
+        'movie': screening.movie_id.title,
+        'layout': layout
     }
 
+    # Renders and returns the template with the context
     template = get_template(template_path)
     html = template.render(context)
-
     return HttpResponse(html)
 
 
@@ -43,7 +63,7 @@ def render_ticket_view(request, *args, **kwargs):
     pk = kwargs.get('pk')
     reservation = get_object_or_404(Reservation, pk=pk)
 
-    #adding data to the ticket template
+    # adding data to the ticket template
     template_path = 'ticket.html'
     context = {
         'name': reservation.user_id.first_name + " " + reservation.user_id.last_name,
@@ -56,7 +76,7 @@ def render_ticket_view(request, *args, **kwargs):
 
     # create file to contain the pdf
     filename = "ticket" + str(pk) + ".pdf"
-    ticket = open("static/customer/tickets/"+filename, "w+b")
+    ticket = open("static/customer/tickets/" + filename, "w+b")
 
     # Find the template and render it
     template = get_template(template_path)
@@ -70,18 +90,17 @@ def render_ticket_view(request, *args, **kwargs):
     if pisa_status.err:
         return HttpResponse('We had some errors <pre>' + html + '</pre>')
 
-
     # emailing ticket to user
     mail = EmailMessage(
         "Ticket(s) for " + reservation.screening_id.movie_id.title +
         " screening " + reservation.screening_id.screening_start.strftime("%H:%M %d/%m/%y"),
         "Dear " + reservation.user_id.first_name + ",\n\nPlease find attached your ticket. Enjoy the show!\n\nPyFilms Inc",
         None,
-        [reservation.user_id.email],)
+        [reservation.user_id.email], )
 
     # attaching the ticket.pdf to the email & sending it
-    mail.attach_file('static/customer/tickets/'+filename)
+    mail.attach_file('static/customer/tickets/' + filename)
     mail.send()
-    
-    #return to customer homepage
+
+    # return to customer homepage
     return HttpResponseRedirect('/customer/')
