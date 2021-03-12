d7876e7fea9490edbe69188a2c65c62e980b0aa4
jakemacgregor <R3b3cc429>
2021-03-12 21:12:40 +0000

    Replaced the existing user table with a new profile table, which extends the built-in user table for added security

--- PyFilmsInc_root/customer/forms.py
+++ PyFilmsInc_root/customer/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
+
 class ReservationForm(forms.Form):
-	# quantities of seats for reservation
-	qAdult = forms.IntegerField(label="qAdult", initial=2)
-	qChild = forms.IntegerField(label="qChild", initial=2)
-	qSenior = forms.IntegerField(label="qSenior", initial=2)
-	
-	# seat numbers
-	
-	# card details
-	
+    # quantities of seats for reservation
+    qAdult = forms.IntegerField(label="qAdult", initial=2)
+    qChild = forms.IntegerField(label="qChild", initial=2)
+    qSenior = forms.IntegerField(label="qSenior", initial=2)
+
+# seat numbers
+
+# card details
