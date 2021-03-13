53c2175988ab0a38f6e8e40195172e3ccf66690e
Alex Craven <sc19amc@leeds.ac.uk>
2021-03-13 15:44:28 +0000

    user can now login & logout

--- PyFilmsInc_root/customer/urls.py
+++ PyFilmsInc_root/customer/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('tickets/<pk>', render_purchase_view, name="buyTickets"),
     path('booking/<pk>', retrieve_make_booking, name="booking"),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
-    #path('logout/$', views.LogoutView, name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(next_page="/customer/"), name="logout"),
 ]
