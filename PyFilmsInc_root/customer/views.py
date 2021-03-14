25459e25622c12a39ee2d52c300c1bb3da34b1c2
jakemacgregor <R3b3cc429>
2021-03-14 12:24:22 +0000

    Added a message that confirms when a booking was successful and displays on the homepage

--- PyFilmsInc_root/customer/views.py
+++ PyFilmsInc_root/customer/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
@@ -12,6 +13,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth import login, authenticate
 
+
 class HomeView(ListView):
     model = Movie
     template_name = 'home.html'
@@ -25,6 +27,7 @@ class MovieDetailView(DetailView):
 class BuyTickets(ListView):
     model = Movie
     template_name = 'buyTickets.html'
+
 
 # generate seat purchasing page
 def render_purchase_view(request, *args, **kwargs):
@@ -68,13 +71,13 @@ def render_ticket_views(screening_id, user_id):
                                               user_id=Profile.objects.get(user=user_id))
     booking = reservations[0]
     tickets = []
-    
+
     # emailing ticket to UserWarning
     mail = EmailMessage(
         "Ticket(s) for " + booking.screening_id.movie_id.title +
         " screening " + booking.screening_id.screening_start.strftime("%H:%M %d/%m/%y"),
         "Dear " + booking.user_id.user.first_name + ",\n\nPlease find attached your ticket. Enjoy the "
-                                                        "show!\n\nPyFilms Inc",
+                                                    "show!\n\nPyFilms Inc",
         None,
         [booking.user_id.user.email], )
 
@@ -111,7 +114,7 @@ def render_ticket_views(screening_id, user_id):
 
         # attaching the ticket.pdf to the email & sending it
         mail.attach_file('static/customer/tickets/' + filename)
-        
+
     mail.send()
 
 
@@ -123,85 +126,88 @@ def retrieve_make_booking(request, *args, **kwargs):
     if request.method == 'POST':
 
         form = ReservationForm(request.POST)
-        reservationCount = 0
-        seatNos = request.POST.get('SelectedSeatsID').split(',')
-        
+        reservation_count = 0
+        seat_nos = request.POST.get('SelectedSeatsID').split(',')
+
         if form.is_valid():
             form = form.cleaned_data
             # make vars
-            qAdult = form["qAdult"]
-            qChild = form["qChild"]
-            qSenior = form["qSenior"]
-            qTotal = qAdult + qChild + qSenior
-            res = Reservation(screening_id=Screening.objects.get(pk=pk), reservation_contact="DoWeNeedThisField?", reserved=True,
+            q_adult = form["qAdult"]
+            q_child = form["qChild"]
+            q_senior = form["qSenior"]
+            q_total = q_adult + q_child + q_senior
+            res = Reservation(screening_id=Screening.objects.get(pk=pk), reservation_contact="DoWeNeedThisField?",
+                              reserved=True,
                               paid=True, cancelled=False, user_id=Profile.objects.get(user=request.user.id))
-            
+
             # update ticket sold quantity for Movie object 
-            Movie.addTickets(qTotal, Screening.objects.get(pk=pk).movie_id)
-            
+            Movie.addTickets(q_total, Screening.objects.get(pk=pk).movie_id)
+
             # create reservation entry per party member
-            for i in range(qTotal):
+            for i in range(q_total):
                 # auto-generate new res object with new pk
                 res.pk = None
                 res.save()
-                
-                if (i < qAdult):
-                    res.reservation_type=Reservation.AD
-                elif (i < (qAdult + qChild)):
-                    res.reservation_type=Reservation.CH
+
+                if i < q_adult:
+                    res.reservation_type = Reservation.AD
+                elif i < (q_adult + q_child):
+                    res.reservation_type = Reservation.CH
                 else:
-                    res.reservation_type=Reservation.SE
-                    
+                    res.reservation_type = Reservation.SE
+
                 res.save()
-                
+
                 # create seat reservation for this party member
-                SeatReserved.objects.create(seat_id=Seat.objects.get(pk=((int(seatNos[i])+541)+(32*(res.screening_id.room_id.name-1)))),
-                                            reservation_id=res, screening_id=Screening.objects.get(pk=pk))
-            
+                SeatReserved.objects.create(
+                    seat_id=Seat.objects.get(pk=((int(seat_nos[i]) + 541) + (32 * (res.screening_id.room_id.name - 1)))),
+                    reservation_id=res, screening_id=Screening.objects.get(pk=pk))
+
             # create transaction entry for reservation (fake card payment)
-            Transaction.objects.create(transaction_type=Transaction.CARD, amount=(qTotal*10),
+            Transaction.objects.create(transaction_type=Transaction.CARD, amount=(q_total * 10),
                                        user_id=Profile.objects.get(user=request.user.id), successful=True)
-             
+
             # render tickets for every member & emails them to customer 
             render_ticket_views(res.screening_id, request.user.id)
 
+    messages.success(request, 'Thanks! Your booking is confirmed, your ticket will arrive in your inbox soon.')
     return HttpResponseRedirect('/customer/')
- 
+
 
 # render user signup page
 def render_signup_view(request):
     if request.method == "POST":
-        
+
         # get user data from html page 
         form = UserCreationForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-                
+
             # validate & add email to user object
             email = request.POST.get('email')
-            if (len(email) > 0):
+            if len(email) > 0:
                 user.email = email
                 user.save()
-            
-            #validate & add name fields
+
+            # validate & add name fields
             name = request.POST.get('first_name')
             surname = request.POST.get('surname')
-            if ((len(name) > 0) and (len(surname) > 0)):
+            if (len(name) > 0) and (len(surname) > 0):
                 user.first_name = name
                 user.last_name = surname
                 user.save()
-                
+
             login(request, user)
             return HttpResponseRedirect('/customer/')
-            
+
     # otherwise generate signup page
     else:
         form = UserCreationForm()
-    
+
     # render page
     template = get_template('signup.html')
     html = template.render({'form': form})
