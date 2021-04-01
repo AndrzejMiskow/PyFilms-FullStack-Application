a32339e7ebfca83bb9ec48cad7345b040a5df86e
Andrej Miskow <sc19am2@leeds.ac.uk>
2021-04-01 01:08:11 +0100

    Fixed seat layout in simulation payment section

--- PyFilmsInc_root/static/business/js/script.js
+++ PyFilmsInc_root/static/business/js/script.js
@@ -1,41 +1,69 @@
-let ctx = document.getElementById('myChart').getContext('2d');
-let labels = ['Incpetion', 'Kung Fu Panda', '1917'];
-let colorHex = ['#3639fb', '#060613', '#43AA8B'];
+//contains all the elements in the seat layout
+const seatLayout = document.querySelector('.seatLayout');
 
-let myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [732 , 521 , 2016],
-            backgroundColor: colorHex
-        }],
-        labels: labels
-    },
-    options: {
-        responsive: true,
-        legend: {
-            position: 'bottom'
-        },
-        plugins: {
-            datalabels: {
-                color: '#fff',
-                anchor: 'end',
-                align: 'start',
-                offset: -10,
-                borderWidth: 2,
-                borderColor: '#fff',
-                borderRadius: 25,
-                backgroundColor: (context) => {
-                    return context.dataset.backgroundColor;
-                },
-                font: {
-                    weight: 'bold',
-                    size: '10'
-                },
-                formatter: (value) => {
-                    return '£' + value;
+//All seats which are not reserved
+const seats = document.querySelectorAll('.row-seat .seat');
+
+//number of tickets
+var totalTickets = 0;
+var selectedSeatsCount = 0;
+
+function takeData()
+{
+    totalTickets = 0;
+    //Adding up tickets from the form
+    totalTickets = Number($("#tChild").val()) + Number($("#tAdult").val()) + Number($("#tSenior").val());
+    if (totalTickets <= 0)
+    {
+        alert("Please select a valid number of tickets");
+    }
+    else
+    {
+        alert("Select Your Seats");
+    }
+}
+
+//Updates the number of selected Seats Can be used to update the database
+function updateSelected (){
+    const selectedSeats = document.querySelectorAll('.row-seat .selected');
+
+    selectedSeatsCount = selectedSeats.length;
+}
+
+function confirmSeats(){
+    const selectedSeats = document.querySelectorAll('.row-seat .selected');
+
+    //We can use seatsIndex to store a seat in the DB as selected (after confirmation)
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    document.getElementById("SelectedSeatsID").value = seatsIndex.toString();
+
+}
+
+
+// Function to select seats, if the number of tickets is bigger than 0
+//The click contains a seat and is not occupied then it will be selected
+seatLayout.addEventListener('click', e => {
+    if (
+        totalTickets > 0 &&
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        if ( selectedSeatsCount < totalTickets){
+            e.target.classList.toggle('selected');
+            updateSelected();
+        }
+        else //if the number of selected seats is greater than the total number of tickets
+            {
+                //We need to deselect a selected seat to be able to allocate another
+                if(e.target.classList.contains('selected')){
+                    e.target.classList.toggle('selected');
+                    updateSelected();
                 }
-            }
+                else{
+                    alert("Deselect A seat to pick a new one");
+                }
         }
     }
-})
+
+});
